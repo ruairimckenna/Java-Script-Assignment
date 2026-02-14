b185a136c9cd4f44473e50c1879d4e14568586f1
ruairimckenna <ruairi.mckenna2@gmail.com>
2026-02-14 23:16:41 +0000

    Operational "Study" Page
    First attempt at an operational study page. The train HTML doesn't exist yet so I need to put in the navs. Also the layout of the page is very basic and the text doesn't look good against the background. However, I'm more concerned with getting the JS operational.

---
+++ script.js
@@ -0,0 +1,34 @@
+// "Study" page JavaScript //
+
+const charts = [
+    "images/ChartP1.png",
+    "images/ChartP2.png",
+    "images/ChartP3.png",
+    "images/ChartP4.png",
+    "images/ChartP5.png",
+
+];
+
+let currentIndex = 0;
+
+const chartImage = document.getElementById("chart-image");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+nextBtn.addEventListener("click", function() {
+    currentIndex = currentIndex + 1;
+
+    if (currentIndex >= charts.length) {
+        currentIndex = 0;
+    }
+    chartImage.src = charts[currentIndex];
+});
+
+prevBtn.addEventListener("click", function() {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+        currentIndex = charts.length - 1;
+    }
+    chartImage.src = charts[currentIndex];
+});
+
