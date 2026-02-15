e540c0f2ee9dc63b1a517fb325cb84f68c560a6b
ruairimckenna <ruairi.mckenna2@gmail.com>
2026-02-15 18:05:55 +0000

    Added the arrays for hands
    Added arrays for random hand generator and the arrays for the correct answers which should match the random hand for play to be clicked. Also changed the chart images because I make a mistake with AT (I called it A10)

--- train.js
+++ train.js
@@ -1,5 +1,95 @@
 // "Train" page JavaScript //
 
+const allHands = [
+"AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+"AKo","KK","KQs","KJs","KTs","K9s","K8s","K7s","K6s","K5s","K4s","K3s","K2s",
+"AQo","KQo","QQ","QJs","QTs","Q9s","Q8s","Q7s","Q6s","Q5s","Q4s","Q3s","Q2s",
+"AJo","KJo","QJo","JJ","JTs","J9s","J8s","J7s","J6s","J5s","J4s","J3s","J2s",
+"ATo","KTo","QTo","JTo","TT","T9s","T8s","T7s","T6s","T5s","T4s","T3s","T2s",
+"A9o","K9o","Q9o","J9o","T9o","99","98s","97s","96s","95s","94s","93s","92s",
+"A8o","K8o","Q8o","J8o","T8o","98o","88","87s","86s","85s","84s","83s","82s",
+"A7o","K7o","Q7o","J7o","T7o","97o","87o","77","76s","75s","74s","73s","72s",
+"A6o","K6o","Q6o","J6o","T6o","96o","86o","76o","66","65s","64s","63s","62s",
+"A5o","K5o","Q5o","J5o","T5o","95o","85o","75o","65o","55","54s","53s","52s",
+"A4o","K4o","Q4o","J4o","T4o","94o","84o","74o","64o","54o","44","43s","42s",
+"A3o","K3o","Q3o","J3o","T3o","93o","83o","73o","63o","53o","43o","33","32s",
+"A2o","K2o","Q2o","J2o","T2o","92o","82o","72o","62o","52o","42o","32o","22"
+];
+
+const p1Hands = [
+"AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+"AKo","KK","KQs","KJs","KTs","K9s","K8s","K7s","K6s","K5s","K4s","K3s","K2s",
+"AQo","KQo","QQ","QJs","QTs","Q9s","Q8s","Q7s","Q6s","Q5s","Q4s",
+"AJo","KJo","QJo","JJ","JTs","J9s","J8s","J7s","J6s",
+"ATo","KTo","QTo","JTo","TT","T9s","T8s","T7s","T6s",
+"A9o","K9o","Q9o","J9o","T9o","99","98s","97s","96s","95s",
+"A8o","K8o","Q8o","J8o","T8o","98o","88","87s","86s","85s","84s",
+"A7o","77","76s","75s","74s",
+"A6o","66","65s","64s","63s",
+"A5o","55","54s","53s",
+"A4o","44","43s",
+"A3o","33",
+"A2o","22"
+];  
+
+const p2Hands = [
+"AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+"AKo","KK","KQs","KJs","KTs","K9s","K8s","K7s","K6s","K5s","K4s","K3s","K2s",
+"AQo","KQo","QQ","QJs","QTs","Q9s","Q8s","Q7s","Q6s","Q5s",
+"AJo","KJo","QJo","JJ","JTs","J9s","J8s","J7s",
+"ATo","KTo","QTo","JTo","TT","T9s","T8s","T7s","T6s",
+"A9o","K9o","Q9o","J9o","T9o","99","98s","97s","96s",
+"A8o","98o","88","87s","86s","85s",
+"A7o","77","76s","75s",
+"A6o","66","65s","64s",
+"A5o","55","54s","53s",
+"A4o","44","43s",
+"33",
+"22"
+];  
+
+const p3Hands = [
+"AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+"AKo","KK","KQs","KJs","KTs","K9s","K8s","K7s","K6s","K5s",
+"AQo","KQo","QQ","QJs","QTs","Q9s","Q8s",
+"AJo","KJo","QJo","JJ","JTs","J9s","J8s",
+"ATo","KTo","QTo","JTo","TT","T9s","T8s",
+"99","98s","97s",
+"88","87s","86s",
+"77","76s","75s",
+"66","65s",
+"55","54s",
+"44","43s",
+"33",
+"22"
+];  
+
+const p4Hands = [
+"AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","A2s",
+"AKo","KK","KQs","KJs","KTs","K9s",
+"AQo","KQo","QQ","QJs","QTs","Q9s",
+"AJo","JJ","JTs","J9s",
+"TT","T9s","T8s",
+"99","98s",
+"88","87s",
+"77","76s",
+"66",
+"55",
+]; 
+
+const p5Hands = [
+"AA","AKs","AQs","AJs","ATs","A5s",
+"AKo","KK","KQs","KJs","KTs",
+"AQo","QQ","QJs","QTs",
+"JJ","JTs","J9s",
+"TT","T9s",
+"99","98s",
+"88",
+"77",
+]; 
+
+
+
 // Table section highlight
 const sections = document.querySelectorAll(".table-section");
 
@@ -14,9 +104,22 @@ sections.forEach(section => {
 });
 
 // Button functionality placeholders
+// Next Hand button aka Random Hand Generator //
 const nextHandBtn = document.getElementById("next-hand");
+const cardDisplay = document.getElementById("card-display");
+
+nextHandBtn.addEventListener("click", function() {
+    const randomIndex = Math.floor(Math.random() * allHands.length);
+    const randomHand = allHands[randomIndex];
+    cardDisplay.textContent = randomHand;
+    console.log("Next Hand clicked: " + randomHand);
+});
+
+// Play and Fold buttons //
+
 const playBtn = document.getElementById("play");
 const foldBtn = document.getElementById("fold");
+
 
 nextHandBtn.addEventListener("click", () => {
     console.log("Next Hand clicked");
