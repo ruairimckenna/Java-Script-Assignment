084c4169cc0bdff1dc3198b73d2fe57f89e4819a
ruairimckenna <ruairi.mckenna2@gmail.com>
2026-02-15 17:11:57 +0000

    Light up P1-P5 fixed
    The buttons at P1 to P5 weren't lighting up because I only had one JS file, which I didn't realise would happen. The console warning told me that it couldn't read the properties of line 18 in the JS file.

--- script.js
+++ script.js
@@ -36,34 +36,3 @@ prevBtn.addEventListener("click", function() {
 
 
 
-// "Train" page JavaScript //
-
-// Table section highlight
-const sections = document.querySelectorAll(".table-section");
-
-sections.forEach(section => {
-    section.addEventListener("click", () => {
-        sections.forEach(s => s.classList.remove("active"));
-        section.classList.add("active");
-
-        // Use for quiz answers later
-
-    });
-});
-
-// Button functionality placeholders
-const nextHandBtn = document.getElementById("next-hand");
-const playBtn = document.getElementById("play");
-const foldBtn = document.getElementById("fold");
-
-nextHandBtn.addEventListener("click", () => {
-    console.log("Next Hand clicked");
-});
-
-playBtn.addEventListener("click", () => {
-    console.log("Play clicked");
-});
-
-foldBtn.addEventListener("click", () => {
-    console.log("Fold clicked");
-});
